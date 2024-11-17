4b21bae289c5aad624b2a637d505a3994afe670e
BorAG8 <borja_agalan@hotmail.es>
2024-11-17 18:06:57 +0100

    Initial commit

---
+++ app/src/main/java/com/example/fragmentsborjaag/Onboarding0Fragment.java
@@ -0,0 +1,58 @@
+package com.example.fragmentsborjaag;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class Onboarding0Fragment extends Fragment {
+
+    Button botonSiguiente;
+    NavController navController;
+
+    Button botonSkip;
+    NavController navController2;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_onboarding0, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        navController = Navigation.findNavController(view);
+
+        botonSiguiente = view.findViewById(R.id.botonSiguiente);
+
+        botonSiguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navController.navigate(R.id.action_onboarding0Fragment_to_onboarding1Fragment);
+            }
+        });
+
+        navController2 = Navigation.findNavController(view);
+
+        botonSkip = view.findViewById(R.id.botonSkip);
+
+        botonSkip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navController2.navigate(R.id.action_onboarding0Fragment_to_homeFragment);
+            }
+        });
+    }
+
+}
